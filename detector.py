716d43f1cd567fdf1f6590969962c8844346b9f0
Malith-19 <malith.19@cse.mrt.ac.lk>
2022-09-23 16:48:37 +0530

    basic humans detection complete

---
+++ detector.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0) #set the input here
+whT = 320
+confThreshold =0.5
+nmsThreshold= 0.2
+
+
+classes = ["person"]
+modelConfiguration = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration,modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs,img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w,h = int(det[2]*wT) , int(det[3]*hT)
+                x,y = int((det[0]*wT)-w/2) , int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+ 
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+    
+    
+    humanDetected = False
+    for i in indices:
+        
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv2.rectangle(img, (x, y), (x+w,y+h), (255, 0 , 255), 2)
+        try:
+            cv2.putText(img,f'{classes[classIds[i]].upper()} {int(confs[i]*100)}%',
+                    (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+            print("humans detected!")
+            humanDetected = True
+        except:
+            print("nothing detected!")
+    return humanDetected
+
+instrusionFrameCollection = [] #frame collection for notify users
+
+while True:
+    success, img = cap.read()
+    
+    blob = cv2.dnn.blobFromImage(img,1/255,(whT,whT),[0,0,0],1,crop=False)
+    net.setInput(blob)
+    
+    layerNames = net.getLayerNames()
+    outputNames = [(layerNames[i - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    humanDetected = findObjects(outputs,img)
+    
+    
+
+        
+ 
+    cv2.imshow('Image', img)
+    key = cv2.waitKey(1)
+    
+    # this is for terminating the program
+    if key == ord("q"):
+        break
+    
