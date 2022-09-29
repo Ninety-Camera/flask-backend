6851dc570ef3aedc35f95f110b2c4f5d0336ff98
Malith-19 <malith.19@cse.mrt.ac.lk>
2022-09-29 22:47:11 +0530

    comments added

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import threading
 from detector import detect, record
 from webConnect import *
 
+# starting the recording. 
 recordingThread = threading.Thread(target=record,name="recorder")
 recordingThread.start()
 
+# starting create connection and listening to connection.
 detectionListeningThread = threading.Thread(target=createConnection,name="instrution-listener")
 detectionListeningThread.start()
 
+#checking for detectBool and starting human detection if it's True.
 while True:
     if detectBool:
         detect()
